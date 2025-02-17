35e75d2ac26b36d9919b2f2de2c2875868cddecf
Charles Ponti <cj@ponti.io>
2025-02-16 18:43:17 -0800

    add debug for redis

--- packages/utils/src/redis.ts
+++ packages/utils/src/redis.ts
@@ -2,7 +2,8 @@ import Redis from "ioredis";
 
 const { REDIS_URL } = process.env;
 if (!REDIS_URL) {
-	throw new Error("Missing REDIS_URL");
+	console.log(process.env);
+	// throw new Error("Missing REDIS_URL");
 }
 
-export const redis = new Redis(REDIS_URL);
+export const redis = new Redis(REDIS_URL || "redis://localhost:6379");
