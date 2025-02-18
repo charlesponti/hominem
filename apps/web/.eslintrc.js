c3509aa096db353ac673ee296153bb60c1677937
Charles Ponti <cj@ponti.io>
2025-02-17 17:41:48 -0800

    fix(eslint): switch from ES module to CommonJS syntax in .eslintrc.js

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,9 +1,5 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const { FlatCompat } = require("@eslint/eslintrc");
+const path = require("node:path");
 
 const compat = new FlatCompat({
 	baseDirectory: __dirname,
@@ -13,4 +9,4 @@ const eslintConfig = [
 	...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+module.exports = eslintConfig;
