75933e4ee46bbc95ac9421d67972da1205adeb32
Charles Ponti <cj@ponti.io>
2025-02-17 17:23:03 -0800

    fix(redis): throw error if REDIS_URL is missing

--- packages/utils/src/redis.ts
+++ packages/utils/src/redis.ts
@@ -2,8 +2,7 @@ import Redis from "ioredis";
 
 const { REDIS_URL } = process.env;
 if (!REDIS_URL) {
-	console.log(process.env);
-	// throw new Error("Missing REDIS_URL");
+	throw new Error("Missing REDIS_URL");
 }
 
 export const redis = new Redis(REDIS_URL || "redis://localhost:6379");
