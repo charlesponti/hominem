b7e3194331ca1d4eee38f17dda64ba541da3d4d0
Charles Ponti <1757624+charlesponti@users.noreply.github.com>
2026-01-27 20:20:32 -0800

    fix: vitest configs

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -3,6 +3,7 @@ export default [
   './apps/notes/vitest.config.ts',
   './apps/rocco/vitest.config.ts',
   './packages/ui/vitest.config.ts',
+  './packages/invites/vitest.config.ts',
   './packages/utils/vitest.config.mts',
   './services/api/vitest.config.mts',
   './services/workers/vitest.config.ts',
