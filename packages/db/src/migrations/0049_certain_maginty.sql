65a8325eb7774bc2fb8a965e9f9e0e919fe091f2
Charles Ponti <1757624+charlesponti@users.noreply.github.com>
2026-01-30 17:59:27 -0800

    fix: remove migration

--- packages/db/src/migrations/0049_certain_maginty.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "transactions" ADD COLUMN "plaid_transaction_id" text;--> statement-breakpoint
-CREATE INDEX "transactions_user_id_idx" ON "transactions" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "transactions_date_idx" ON "transactions" USING btree ("date");--> statement-breakpoint
-CREATE INDEX "transactions_account_id_idx" ON "transactions" USING btree ("account_id");--> statement-breakpoint
-ALTER TABLE "transactions" ADD CONSTRAINT "transactions_plaid_transaction_id_unique" UNIQUE("plaid_transaction_id");
